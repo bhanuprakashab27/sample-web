aec68ca871933faccf87a4f147ff86a1154c40e8
Bhanuprakasha B <127314672+bhanuprakashab27@users.noreply.github.com>
2026-02-14 01:58:31 +0530

    sample-web uploading

---
+++ js/main.js
@@ -0,0 +1,99 @@
+function toggleMenu(){
+  document.getElementById("navLinks").classList.toggle("show");
+}
+
+// slider only if exists
+const slides = document.getElementById("slides");
+if(slides){
+  let index = 0;
+  const totalSlides = slides.children.length;
+
+  function showSlide(){
+    slides.style.transform = `translateX(-${index*100}%)`;
+  }
+
+  window.nextSlide = function(){
+    index = (index+1) % totalSlides;
+    showSlide();
+  }
+
+  window.prevSlide = function(){
+    index = (index-1+totalSlides) % totalSlides;
+    showSlide();
+  }
+
+  setInterval(nextSlide,4000);
+}
+
+// What we offer
+
+let offerIndex = 0;
+autoOffer();
+
+function showOffer(index){
+  const tabs = document.querySelectorAll(".offer-tab");
+  const boxes = document.querySelectorAll(".offer-box");
+
+  tabs.forEach(t => t.classList.remove("active"));
+  boxes.forEach(b => b.classList.remove("active"));
+
+  tabs[index].classList.add("active");
+  boxes[index].classList.add("active");
+
+  offerIndex = index;
+}
+
+function autoOffer(){
+  const total = document.querySelectorAll(".offer-tab").length;
+
+  setInterval(()=>{
+    offerIndex++;
+    if(offerIndex >= total) offerIndex = 0;
+    showOffer(offerIndex);
+  },6000);
+}
+
+// admission process
+
+ const steps = document.querySelectorAll(".step");
+  const progressFill = document.querySelector(".progress-fill");
+
+  let currentStep = 0;
+
+  function activateStep() {
+    if (currentStep < steps.length) {
+      steps[currentStep].classList.add("active");
+      progressFill.style.height = 
+        ((currentStep + 1) / steps.length) * 100 + "%";
+      currentStep++;
+    }
+  }
+
+  setInterval(activateStep, 1000);
+
+
+  // gallery
+
+
+const buttons = document.querySelectorAll(".filter-btn");
+const items = document.querySelectorAll(".gallery-item");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    document.querySelector(".filter-btn.active").classList.remove("active");
+    btn.classList.add("active");
+
+    const filter = btn.getAttribute("data-filter");
+
+    items.forEach(item => {
+      if (filter === "all" || item.getAttribute("data-category") === filter) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+
+  });
+});
+
